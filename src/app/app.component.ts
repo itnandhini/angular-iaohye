6996a52bfa5f13b57567e04b92bc4d5ed7cdcd69
itnandhini <itnandhini8@gmail.com>
2019-12-06 15:10:05 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,111 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators, FormArray } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  name = "Angular";
   form: FormGroup;
   ACCEPTABILITY = Acceptability;
-
+  testArray = [];
+  stopEvent = true;
   constructor(private fb: FormBuilder) {}
 
+  users: Array<any> = [
+    {
+      id: 1,
+      ffid: 0,
+      name: "Sunday",
+      active: false
+    },
+    {
+      id: 2,
+      name: "Monday",
+      active: false
+    },
+    {
+      id: 3,
+      name: "Tuesday",
+      active: false
+    },
+    {
+      id: 4,
+      name: "Wednesday",
+      active: false
+    },
+    {
+      id: 5,
+      name: "Thusday",
+      active: false
+    },
+    {
+      id: 6,
+      name: "Friday",
+      active: false
+    },
+    {
+      id: 7,
+      name: "Saturday",
+      active: false
+    }
+  ];
   ngOnInit() {
     // build the form
     this.form = this.fb.group({
       days: this.fb.array([])
     });
-
   }
 
   get days(): FormArray {
-    return this.form.get('days') as FormArray;
+    return this.form.get("days") as FormArray;
   }
 
   addCondition() {
-    this.days.push(this.fb.group({
-      days: ['', Validators.required]
-    }));
-    console.log("conditions", this.days)
+    this.days.push(
+      this.fb.group({
+        days: ["", Validators.required]
+      })
+    );
+    console.log("conditions", this.days);
+  }
+  clickMe(value) {
+    console.log(value);
+    this.testArray = [];
+    switch (value) {
+      case "All":
+        console.log("All");
+        this.stopEvent = true;
+        break;
+      case "Five":
+        console.log("five");
+        this.stopEvent = true;
+        break;
+      case "Two":
+        this.testArray = [1, 7];
+
+        console.log("two");
+        this.stopEvent = true;
+        break;
+      case "Any":
+        console.log("any");
+        this.stopEvent = !this.stopEvent;
+        break;
+    }
+  }
+    click(user, id) {
+    if (this.testArray.indexOf(user.name) !== -1) {
+      var index = this.testArray.indexOf(user.name);
+      this.testArray.splice(index, 1);
+      alert(this.testArray);
+    } else {
+      this.testArray.push(user.name);
+      alert(this.testArray);
+    }
+    user.active = !user.active;
+    // your code here....
   }
 }
 export enum Acceptability {
@@ -40,6 +116,5 @@ export enum Acceptability {
 }
 
 export interface Condition {
-  title: string;
   days: Acceptability;
 }
